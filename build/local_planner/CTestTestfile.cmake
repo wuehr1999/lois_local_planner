d37012c5783227e00862762635d801b8de465ef2
wuehr1999 <jonas.wuehr@t-online.de>
2023-04-24 19:41:58 +0200

    one step more to ros2

--- build/local_planner/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jonas/loisWS/lois_local_planner/local_planner
-# Build directory: /home/jonas/loisWS/lois_local_planner/build/local_planner
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
